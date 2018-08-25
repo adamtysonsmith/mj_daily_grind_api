442c1b4289bc1d3ccc4415b4d67ae092aeb7cd31
Adam <atsmith227@gmail.com>
2018-08-24 20:30:48 -0600

    cors config

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // environment config
 require('dotenv').config()
 
-// init express app
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const initRoutes = require('./routes')
 const port = process.env.PORT || '3000'
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.APP_URL) callback(null, true)
+    else callback(new Error('Not allowed by CORS'))
+  }
+}
+
+// init express app & middleware
 const app = express()
 app.use(bodyParser.json())
+app.use(cors(corsOptions))
 
 // register routes
 initRoutes(app)
